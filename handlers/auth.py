61254c2d77d7dea400bd1726d3ce8595430a6d5a
krishtalevia <krishtalev.ilya@gmail.com>
2024-12-22 16:18:04 +0300

    add start handler, del reg check

--- handlers/auth.py
+++ handlers/auth.py
@@ -5,7 +5,20 @@ from aiogram.fsm.state import StatesGroup, State
 
 from model import DatabaseManager
 
+router = Router()
+db_manager = DatabaseManager()
+
 class AuthStates(StatesGroup):
-    registration_check = State()
     waiting_for_username = State()
     registered = State()
+
+@router.message(StateFilter(None), Command('start'))
+async def start_handler(message: types.Message, state: FSMContext):
+    telegram_id = message.from_user.id
+    if db_manager.get_user(telegram_id) is not None:
+        await state.set_state(AuthStates.registered)
+        await message.answer('Добро пожаловать. Снова.')
+        # Вывод вариантов команд
+    else:
+        await state.set_state(AuthStates.waiting_for_username)
+        await message.answer('Введите имя пользователя для регистрации:')
