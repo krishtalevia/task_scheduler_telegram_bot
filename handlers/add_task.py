1daf1948bcc19c66266117add17c8ab4c1aeb461
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-02 15:20:45 +0300

    rename tasks.py -> add_task.py

---
+++ handlers/add_task.py
@@ -0,0 +1,9 @@
+from aiogram import Router, types
+from aiogram.filters import StateFilter, Command, CommandObject
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+
+from model import DatabaseManager
+
+router = Router()
+db_manager = DatabaseManager()
