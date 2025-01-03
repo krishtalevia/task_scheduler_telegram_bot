8431c382ca539dcf03be5c407427a2921c8a87ad
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-03 21:32:59 +0300

    feat: show tasks, empty view task handler

--- handlers/view_tasks.py
+++ handlers/view_tasks.py
@@ -9,3 +9,27 @@ from model import DatabaseManager, Task
 
 router = Router()
 db_manager = DatabaseManager()
+
+@router.message(StateFilter(AuthStates.authorized), Command('view_tasks'))
+async def view_tasks_handler(message: types.Message, state: FSMContext):
+    pass
+
+def show_tasks(tasks):
+    if not tasks:
+        return 'Задачи не найдены.'
+    
+    result = []
+    for task in tasks:
+        title = task['title']
+        description = task['description']
+        deadline = task['deadline']
+        priority = task['priority']
+
+        result.append(
+            f"📌 {title}\n"
+            f"📖 {description}\n"
+            f"📅 {deadline}\n"
+            f"🎯 {priority}\n"
+        )
+
+    return '\n'.join(result)
