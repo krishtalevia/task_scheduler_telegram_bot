cf525ed45c78a21004790be005df8211352d0225
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-05 17:24:25 +0300

    del AuthStates, add reg/auth from db

--- handlers/auth.py
+++ handlers/auth.py
@@ -1,32 +1,31 @@
 from aiogram import Router, types
-from aiogram.filters import StateFilter, Command, CommandObject
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
+from aiogram.filters import Command
 
 from model import DatabaseManager
 
 router = Router()
 db_manager = DatabaseManager()
 
-@router.message(StateFilter(None), Command('register'))
-async def register_handler(message: types.Message, state: FSMContext):    
+@router.message(Command('register'))
+async def register_handler(message: types.Message):    
     telegram_id = message.from_user.id
     if db_manager.get_user(telegram_id) is None:
         if db_manager.register_user(telegram_id):
-            await message.answer('Вы успешно зарегистрировались. Для авторизации используйте /login')
+            await message.answer('✅ Вы успешно зарегистрировались. Для авторизации используйте /login')
         else:
-           await message.answer('Во время регистрации произошла ошибка.')
+           await message.answer('❌ Во время регистрации произошла ошибка.')
     else:
-        await message.answer('Вы уже зарегистрированы. Используйте /login для авторизации')
+        await message.answer('⚠️ Вы уже зарегистрированы. Используйте /login для авторизации')
 
-@router.message(StateFilter(None), Command('login'))
-async def login_handler(message: types.Message, state: FSMContext):
+@router.message(Command('login'))
+async def login_handler(message: types.Message):
     telegram_id = message.from_user.id
-    current_state = await state.get_state()
-    if db_manager.get_user(telegram_id) is None:
-        await message.answer('Пользователь не найден. Для регистрации используйте /register')
-    elif current_state == AuthStates.authorized.state:
-        await message.answer('Вы уже авторизованы.')
+
+    user = db_manager.get_user(telegram_id)
+    if user is None:
+        await message.answer('❌ Пользователь не найден. Для регистрации используйте /register')
+    elif db_manager.is_user_authorized(telegram_id):
+        await message.answer('⚠️ Вы уже авторизованы.')
     else:
-        await state.set_state(AuthStates.authorized)
-        await message.answer('Вы успешно авторизованы.')
+        db_manager.authorize_user(telegram_id)
+        await message.answer('✅ Вы успешно авторизованы.')
