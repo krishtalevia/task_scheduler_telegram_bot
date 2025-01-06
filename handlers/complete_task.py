0a8e556390b3c4a2c5dac607e6e35df2cbba775a
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-06 16:44:49 +0300

    add FSM imports, router/db, empty feat complete task handler

--- handlers/complete_task.py
+++ handlers/complete_task.py
@@ -1,4 +1,14 @@
 from aiogram import Router, types
-from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import StateFilter, Command
+from aiogram.fsm.state import StatesGroup, State
 
 from model import DatabaseManager
+
+
+router = Router()
+db_manager = DatabaseManager()
+
+@router.message(StateFilter(None), Command('complete_task'))
+async def complete_task_handler(message: types.Message, state: FSMContext):
+    pass
