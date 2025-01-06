d91499d9e570241f383b866880fc1ef4e442ca5a
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-06 14:14:23 +0300

    create edit_task.py, add imports, add feat edit task handler

---
+++ handlers/edit_task.py
@@ -0,0 +1,18 @@
+from aiogram import Router, types
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import StateFilter, Command
+from aiogram.fsm.state import StatesGroup, State
+
+from model import DatabaseManager
+
+class EditTaskStates(StatesGroup):
+    ChoosingParameter = State()
+    EditinParameter = State()
+
+router = Router()
+db_manager = DatabaseManager()
+
+@router.message(Command('edit_task'))
+async def edit_task_handler(message: types.Message, state: FSMContext)
+    await message.answer('Введите ID задачи, которую хотите изменить:')
+    await state.set_state(EditTaskStates.ChoosingParameter)
