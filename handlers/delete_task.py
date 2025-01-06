7ab234d61d387602d0cccce3feaedd88df69f6ce
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-06 19:21:33 +0300

    create delete_task.py, add imports, FSM

---
+++ handlers/delete_task.py
@@ -0,0 +1,12 @@
+from aiogram import Router, types
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import StateFilter, Command
+from aiogram.fsm.state import StatesGroup, State
+
+from model import DatabaseManager
+
+router = Router()
+db_manager = DatabaseManager()
+
+class CompleteTaskStates(StatesGroup):
+    Completing = State()
