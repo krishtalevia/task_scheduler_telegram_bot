9135602e92cf808f73c70ba710bbe6c4eebae7e1
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-06 16:53:00 +0300

    add FSM imports, States, feat complete task handler

--- handlers/complete_task.py
+++ handlers/complete_task.py
@@ -5,10 +5,14 @@ from aiogram.fsm.state import StatesGroup, State
 
 from model import DatabaseManager
 
-
 router = Router()
 db_manager = DatabaseManager()
 
+class CompleteTaskStates(StatesGroup):
+    WaitingForTaskId = State()
+    CheckingTaskExistence = State()
+
 @router.message(StateFilter(None), Command('complete_task'))
 async def complete_task_handler(message: types.Message, state: FSMContext):
-    pass
+    await message.answer('Введите ID задачи, которую требуется пометить как выполненную:')
+    await state.set_state(CompleteTaskStates.WaitingForTaskId)
