c81475f8f3cbf61cf68d3c4bf0f0441850fd5e73
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-06 17:21:02 +0300

    add import complete task, dp include

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import TOKEN
-from handlers import start, add_task, auth, view_tasks, edit_task
+from handlers import start, add_task, auth, view_tasks, edit_task, complete_task
 
 async def main():
     bot = Bot(token=TOKEN) 
@@ -14,6 +14,7 @@ async def main():
     dp.include_routers(add_task.router)
     dp.include_routers(view_tasks.router)
     dp.include_router(edit_task.router)
+    dp.include_router(complete_task.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
