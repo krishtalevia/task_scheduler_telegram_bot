e8f4f87c6116f7736b80150aac5a5f50f42cd5e5
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-06 17:05:55 +0300

    feat: completing handler

--- handlers/complete_task.py
+++ handlers/complete_task.py
@@ -9,10 +9,29 @@ router = Router()
 db_manager = DatabaseManager()
 
 class CompleteTaskStates(StatesGroup):
-    WaitingForTaskId = State()
-    CheckingTaskExistence = State()
+    Completing = State()
 
 @router.message(StateFilter(None), Command('complete_task'))
 async def complete_task_handler(message: types.Message, state: FSMContext):
     await message.answer('Введите ID задачи, которую требуется пометить как выполненную:')
-    await state.set_state(CompleteTaskStates.WaitingForTaskId)
+    await state.set_state(CompleteTaskStates.Completing)
+
+@router.message(StateFilter(CompleteTaskStates.Completing))
+async def completing_handler(message: types.Message, state: FSMContext):
+    telegram_id = message.from_user.id
+    task_id = message.text()
+    task = db_manager.get_task_by_id(task_id)
+
+    if task:
+        if task[6] == 'Выполнена':
+            await message.answer(f'⚠️ Задача с ID {task_id} уже выполнена.')
+
+        elif task[6] == 'Не выполнена':
+            if db_manager.update_task(telegram_id, task_id, parameter_name='status', new_value=True):
+                await message.answer(f'🎉 Задача с ID {task_id} теперь выполнена.')
+            else:
+                await message.answer('⚠️ При выполнении операции произошла ошибка.')
+    else:
+        await message.answer(f'⚠️ Задача с ID {task_id} не найдена.')
+    
+    await state.clear()
