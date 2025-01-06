2e22404a6922daeb73244161003138e0930329d9
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-06 14:28:01 +0300

    feat: choosing parameter handler

--- handlers/edit_task.py
+++ handlers/edit_task.py
@@ -16,3 +16,15 @@ db_manager = DatabaseManager()
 async def edit_task_handler(message: types.Message, state: FSMContext):
     await message.answer('Введите ID задачи, которую хотите изменить:')
     await state.set_state(EditTaskStates.ChoosingParameter)
+
+@router.message(EditTaskStates.ChoosingParameter)
+async def choosing_parameter_handler(message: types.Message, state: FSMContext):
+    telegram_id = message.from_user.id
+    task_id = message.text.strip()
+
+    task = db_manager.get_task_by_id(telegram_id, task_id)
+
+    if not task:
+        await message.answer('❌ Задача с таким ID не найдена.')
+        await state.clear()
+        return
