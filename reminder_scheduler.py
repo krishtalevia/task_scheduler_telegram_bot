cca9570fdda26a7efe59ac1d46d3f90db38f1743
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-07 15:16:12 +0300

    feat: reminder_schedule

--- reminder_scheduler.py
+++ reminder_scheduler.py
@@ -6,36 +6,35 @@ from model import DatabaseManager
 
 db_manager = DatabaseManager()
 
-async def reminder_schedule(dp):
+async def reminder_schedule(bot):
     while True:
         try: 
-            current_time = datetime.datetime.now()
+            current_time_no_ms = datetime.datetime.now().replace(microsecond=0)
 
-            connection = sqlite3.connect('database.db')
-            cursor = connection.cursor()
+            with sqlite3.connect('database.db') as connection:
+                cursor = connection.cursor()
 
-            cursor.execute('''
-                SELECT tasks.title, tasks.deadline, users.telegram_id, users.reminder_time
-                FROM tasks
-                JOIN users ON tasks.user_id = users.id
-                WHERE tasks.status = 0
-            ''')
-            tasks_to_remind = cursor.fetchall()
-            connection.close()
+                cursor.execute('''
+                    SELECT tasks.title, tasks.deadline, users.telegram_id, users.reminder_time
+                    FROM tasks
+                    JOIN users ON tasks.user_id = users.telegram_id
+                    WHERE tasks.status = 0
+                ''')
+                tasks_to_remind = cursor.fetchall()
 
             for title, deadline, telegram_id, reminder_time in tasks_to_remind:
-                reminder_time_delta = datetime.timedelta(hours=reminder_time)
-                reminder_time_point = deadline - reminder_time_delta
+                deadline = datetime.datetime.strptime(deadline, '%Y-%m-%d %H:%M:%S')
+                time_left = deadline - current_time_no_ms
 
-                if reminder_time_point <= current_time < deadline:
-                    minutes_left = (deadline - current_time).seconds // 60
-                    await dp.send_message(
+                if time_left == datetime.timedelta(hours=int(reminder_time)):
+                    minutes_left = (deadline - current_time_no_ms).seconds // 60
+                    await bot.send_message(
                         chat_id=telegram_id,
                         text=f'⏳ Напоминание! До задачи "{title}" осталось {minutes_left} минут.'
                         )
                 
-            await asyncio.sleep(60)
+            await asyncio.sleep(1)
         
         except Exception:
             print('Произошла ошибка в планировщике напоминаний.')
-            await asyncio.sleep(60)
+            await asyncio.sleep(1)
