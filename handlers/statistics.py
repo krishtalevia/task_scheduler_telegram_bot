bdfd0cb5e9cc336f2a7fd2a967429e5391cb0aa4
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-07 19:18:43 +0300

    create feat statistics handler

--- handlers/statistics.py
+++ handlers/statistics.py
@@ -7,3 +7,21 @@ from model import DatabaseManager
 
 router = Router()
 db_manager = DatabaseManager()
+
+@router.message(Command('statistics'))
+async def statistics_handler(message: types.Message, command: CommandObject):
+    telegram_id = message.from_user.id
+    args = command.args
+
+    if not db_manager.get_user(telegram_id):
+        await message.answer('❌ Вы не зарегистрированы. Пожалуйста, зарегистрируйтесь.')
+        return
+    
+    if not db_manager.is_user_authorized(telegram_id):
+        await message.answer('❌ Вы не авторизованы. Пожалуйста, авторизуйтесь.')
+        return
+    
+    tasks = db_manager.get_tasks(telegram_id)
+
+    if not args:
+        pass
