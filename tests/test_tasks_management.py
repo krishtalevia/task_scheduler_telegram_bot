772b1246e48324ad7e3c1f203dae608298172bc8
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-08 18:59:30 +0300

    del everything, add imports, test db

--- tests/test_tasks_management.py
+++ tests/test_tasks_management.py
@@ -1,30 +1,11 @@
 import pytest
 import datetime
-
-from aiogram import types
-from aiogram.fsm.context import FSMContext
-from handlers.add_task import add_task_handler, adding_title_handler, adding_description_handler, adding_deadline_handler, adding_priority_handler, task_adding_confirmation
 from model import DatabaseManager, init_db
 
-from model import DatabaseManager, init_db
-
-@pytest.fixture
-def db_manager():
-    db_manager = DatabaseManager()
+@pytest.fixture(scope='module')
+def test_db():
     init_db()
+    db_manager = DatabaseManager('database.db')
     yield db_manager
+    db_manager.delete_all_users()
     db_manager.close()
-
-@pytest.fixture
-def fsm_context():
-    return FSMContext()
-
-@pytest.fixture
-def message():
-    return types.Message()
-
-@pytest.fixture
-def register_user(db_manager):
-    telegram_id = 123
-    db_manager.register_user(telegram_id)
-    return telegram_id
