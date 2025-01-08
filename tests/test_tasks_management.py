069a5ea8d5fc3e9eebced89c1a03203963ebc5dc
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-08 19:25:00 +0300

    feat: test_edit_task_deadline

--- tests/test_tasks_management.py
+++ tests/test_tasks_management.py
@@ -33,3 +33,19 @@ def test_add_task(test_db, test_user):
     assert tasks is not None
     task = tasks[0]
     assert task[2] == title
+
+def test_edit_task_deadline(test_db, test_user):
+    telegram_id = test_user
+    title = 'Тестовая задача'
+    description = 'Тестовое описание'
+    deadline = datetime.datetime.now() + datetime.timedelta(hours=1)
+    priority = 'Высокий'
+    created_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    new_deadline = datetime.datetime.strptime('2025-01-10 18:00:00', '%Y-%m-%d %H:%M:%S')
+
+    test_db.add_task(telegram_id, title, description, deadline, priority, created_at)
+
+    test_db.update_task(telegram_id, 1, 'deadline', new_deadline)
+
+    task = test_db.get_task_by_id(telegram_id, 1)
+    assert task[4] == '2025-01-10 18:00:00'
