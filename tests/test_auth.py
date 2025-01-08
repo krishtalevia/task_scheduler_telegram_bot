ce5f1c7c7fcbb358917f4add15e629dd9c325b6a
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-08 17:02:06 +0300

    feat: auth registered user, fix others

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,7 +9,11 @@ from handlers import auth
 def db_manager():
     db_manager = DatabaseManager()
     init_db()
-    return db_manager
+    db_manager.delete_all_users()
+
+    yield db_manager
+    
+    db_manager.close()
 
 # Тестовый случай 1.1: Попытка регистрации уже зарегистрированного пользователя
 def test_register_existing_user(db_manager):
@@ -18,7 +22,6 @@ def test_register_existing_user(db_manager):
 
     with pytest.raises(ValueError, match='Пользователь уже существует!'):
         db_manager.register_user(telegram_id)
-        db_manager.delete_user(telegram_id)
 
 # Тестовый случай 1.2: Успешная регистрация нового пользователя
 def test_register_new_user(db_manager):
@@ -27,5 +30,13 @@ def test_register_new_user(db_manager):
     result = db_manager.register_user(telegram_id)
     assert result is True
     assert db_manager.get_user(telegram_id) is not None
-    db_manager.delete_user(telegram_id)
 
+# Тестовый случай 1.3: Авторизация зарегистрированного пользователя
+def auth_registered_user(db_manager):
+    telegram_id = 3
+    db_manager.register_user(telegram_id)
+
+    result = db_manager.authorize_user(telegram_id)
+    assert result is True
+
+    assert db_manager.is_user_authorized(telegram_id) is True
