c4222e1c6de7aa9f495c3af034d0526bd7c4c8f8
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-08 17:08:50 +0300

    create test tasks management.py, add imports, fixture

---
+++ tests/test_tasks_management.py
@@ -0,0 +1,16 @@
+import pytest
+import datetime
+
+from aiogram import types
+from aiogram.fsm.context import FSMContext
+from handlers.add_task import add_task_handler, adding_title_handler, adding_description_handler, adding_deadline_handler, adding_priority_handler, task_adding_confirmation
+from model import DatabaseManager, init_db
+
+from model import DatabaseManager, init_db
+
+@pytest.fixture
+def db_manager():
+    db_manager = DatabaseManager()
+    init_db()
+    yield db_manager
+    db_manager.close()
