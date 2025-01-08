c2cd4c171d998169498f1227370bd5a4db86675c
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-08 20:59:26 +0300

    add fixtures

--- tests/test_view_tasks.py
+++ tests/test_view_tasks.py
@@ -11,3 +11,19 @@ from handlers.view_tasks import (
     sort_tasks_by_status,
     show_tasks
 )
+
+@pytest.fixture(scope='module')
+def test_db():
+    init_db()
+    db_manager = DatabaseManager('database.db')
+    yield db_manager
+    db_manager.delete_all_users()
+    db_manager.close()
+
+@pytest.fixture(scope='module')
+def test_user(test_db):
+    telegram_id = 1
+    test_db.register_user(telegram_id)
+    test_db.authorize_user(telegram_id)
+    yield telegram_id
+    test_db.delete_user(telegram_id)
