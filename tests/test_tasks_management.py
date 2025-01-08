c4c533c732bed2e20130f22c556ab71f932f7d81
krishtalevia <krishtalev.ilya@gmail.com>
2025-01-08 20:14:16 +0300

    feat: test delete task

--- tests/test_tasks_management.py
+++ tests/test_tasks_management.py
@@ -45,10 +45,10 @@ def test_edit_task_deadline(test_db, test_user):
 
     test_db.add_task(telegram_id, title, description, deadline, priority, created_at)
 
-    test_db.update_task(telegram_id, 1, 'deadline', new_deadline)
+    test_db.update_task(telegram_id, 2, 'deadline', new_deadline)
 
-    task = test_db.get_task_by_id(telegram_id, 1)
-    assert task[4] == '2025-01-10 18:00:00'
+    tasks = test_db.get_tasks(telegram_id)
+    assert tasks[0][4] == '2025-01-10 18:00:00'
 
 def test_edit_task_priority(test_db, test_user):
     telegram_id = test_user
@@ -61,7 +61,24 @@ def test_edit_task_priority(test_db, test_user):
 
     test_db.add_task(telegram_id, title, description, deadline, priority, created_at)
 
-    test_db.update_task(telegram_id, 1, 'priority', new_priority)
+    test_db.update_task(telegram_id, 3, 'priority', new_priority)
 
-    task = test_db.get_task_by_id(telegram_id, 1)
-    assert task[5] == 'Средний'
+    task = test_db.get_task_by_id(telegram_id, 3)
+    assert task[5] == new_priority
+
+def test_delete_task(test_db, test_user):
+    telegram_id = test_user
+    title = 'Тестовая задача'
+    description = 'Тестовое описание'
+    deadline = datetime.datetime.now() + datetime.timedelta(hours=1)
+    priority = 'Высокий'
+    created_at = datetime.datetime.now()
+
+    test_db.add_task(telegram_id, title, description, deadline, priority, created_at)
+
+    task = test_db.get_task_by_id(telegram_id, 4)
+    assert task is not None
+
+    test_db.delete_task(telegram_id, 4)
+    task = test_db.get_task_by_id(telegram_id, 4)
+    assert task is None
